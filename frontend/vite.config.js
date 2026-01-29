b62c383a5acdc15d6ea62f2ef8d3f85285f426f5
Leonardo Hasson <hassonleonardo@gmail.com>
2026-01-29 00:45:07 -0300

    Permitir dominio de Railway en Vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    host: true, // Esto permite que escuche en todos los puertos (vital para la nube)
+    allowedHosts: ['cozy-consideration-production.up.railway.app'] // <--- ¡AQUÍ ESTÁ LA CLAVE!
+  }
 })
