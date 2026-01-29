4e1c8b1c84573ff6f85a165b420c57b1bac8d585
Leonardo Hasson <hassonleonardo@gmail.com>
2026-01-29 14:41:02 -0300

    Arreglo CORS y vuelvo a ddl-auto update

--- src/main/java/com/cardiologico/proyecto/cordio/config/SecurityConfig.java
+++ src/main/java/com/cardiologico/proyecto/cordio/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.cardiologico.proyecto.cordio.config;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
@@ -8,6 +10,9 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import lombok.RequiredArgsConstructor;
 
@@ -22,18 +27,29 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-            .csrf(csrf -> csrf.disable()) // Deshabilitamos CSRF porque usaremos Tokens
+            .csrf(csrf -> csrf.disable()) 
+            .cors(cors -> cors.configurationSource(corsConfigurationSource())) 
             .authorizeHttpRequests(authRequest -> 
               authRequest
-                // Rutas Publicas (Login y Registro)
                 .requestMatchers("/api/auth/**").permitAll()
-                // El resto requiere autenticacion
-                .anyRequest().authenticated()
+                .anyRequest().authenticated() 
             )
             .sessionManagement(sessionManager ->
                 sessionManager.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authenticationProvider(authProvider)
             .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
             .build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("*")); // Permite entrar desde CUALQUIER lugar (Tu Frontend)
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS")); // Permite todas las acciones
+        configuration.setAllowedHeaders(List.of("Authorization", "Content-Type")); // Permite enviar el Token
+        
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
