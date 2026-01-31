3559596b029d6e2fdcd777394a20aeaedd264027
Leonardo Hasson <hassonleonardo@gmail.com>
2026-01-31 19:37:23 -0300

    Update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,7 @@ import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 
-// Iconos Lucide (Incluye los nuevos para el dashboard)
+// Iconos Lucide (Incluye los nuevos para el dashboard).
 import { 
   Plus, 
   Search, 
